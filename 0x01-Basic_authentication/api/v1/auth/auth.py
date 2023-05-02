a3f67007b47424f5d698dddb8ec60fa6c66d5918
SifisoSithole <103381999+SifisoSithole@users.noreply.github.com>
2023-05-02 11:08:10 +0000

    task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -24,14 +24,26 @@ class Auth:
         Arguments
         ---------
             path (str):
-            execluded_Paths (List[str]):
+            excluded_paths (List[str]):
 
         Return
         ------
             (bool):
         """
 
-        return False
+        if path is None:
+            return True
+
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        if path[-1] != '/':
+            path += '/'
+
+        if path in excluded_paths:
+            return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
