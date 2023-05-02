022de6064e794b1cbc4c80d551ce9eed44242f45
SifisoSithole <103381999+SifisoSithole@users.noreply.github.com>
2023-05-02 10:12:07 +0000

    task 1

--- 0x01-Basic_authentication/SimpleAPI/api/v1/app.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/app.py
@@ -7,6 +7,7 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from werkzeug.exceptions import Unauthorized
 
 
 app = Flask(__name__)
@@ -19,6 +20,14 @@ def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
+
+@app.errorhandler(Unauthorized)
+def unauthorized(error: Unauthorized) -> str:
+    """
+    unauthorized acccess
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
 
 
 if __name__ == "__main__":
