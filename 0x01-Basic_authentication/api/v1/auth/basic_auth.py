ea0453c0239789ca11fccd64d699fd972c0bf1ee
SifisoSithole <103381999+SifisoSithole@users.noreply.github.com>
2023-05-02 14:51:46 +0000

    task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,24 @@ class BasicAuth(Auth):
     """
     Basic Authorization module
     """
-    pass
+
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str
+            ) -> str:
+        """
+        extracts the username and password in base64
+
+        Arguments:
+            authorization_header (str): authorixation header
+
+        Return:
+            (str): the value after Basic (after the space)
+        """
+        if not authorization_header or type(authorization_header) is not str:
+            return None
+
+        authorization_header = authorization_header.split()
+        if authorization_header[0] != 'Basic':
+            return None
+        return authorization_header[1]
