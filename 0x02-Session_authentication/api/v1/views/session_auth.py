367f6c1e61caebcdd786723c48f5588cf56dfd24
SifisoSithole <103381999+SifisoSithole@users.noreply.github.com>
2023-05-04 11:49:06 +0000

    task 8

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -31,7 +31,12 @@ def log():
     else:
         return jsonify({"error": "wrong password"}), 401
 
-@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+
+@app_views.route(
+    '/auth_session/logout',
+    methods=['DELETE'],
+    strict_slashes=False
+)
 def logout():
     """
     delete a session
