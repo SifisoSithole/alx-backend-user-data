1f521fb748c3204c30203aeff1b62a7a4f7d0bd4
SifisoSithole <103381999+SifisoSithole@users.noreply.github.com>
2023-05-11 11:11:29 +0000

    task 14

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -61,6 +61,7 @@ def logout():
     if not session_id:
         abort(403)
     user = AUTH.get_user_from_session_id(session_id)
+    print(user)
     if not user:
         abort(403)
     AUTH.destroy_session(user.id)
