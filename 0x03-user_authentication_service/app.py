6f4ab215296113c6203d9863b6607cb49898fda8
SifisoSithole <103381999+SifisoSithole@users.noreply.github.com>
2023-05-11 11:36:25 +0000

    task 15

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -63,5 +63,18 @@ def logout():
     return redirect(url_for('entry'))
 
 
+@app.route('/profile', methods=['GET'])
+def profile():
+    """
+    returns user profile
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        return jsonify({"email": f"{user.email}"})
+    else:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
